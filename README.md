3c9fcc735a8275ecd28754e626ade8dc2a4fffa6
Davi <davimmorales@gmail.com>
2017-04-27 20:51:25 -0300

    it has to start somewhere

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
 # arduino-calc
-An arduino based calculator.
---
+++ ctest.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "string.h"
+
+
+int main(int argc, char const *argv[]) {
+  char expression[16];
+  scanf("%s", expression);
+  printf("%s\n", expression);
+
+  
+  return 0;
+}
